9af7c699c74f8d48a3e06d8b0089141217e4dbae
NataliyaPlat <60834307+NataliyaPlat@users.noreply.github.com>
2020-02-29 09:17:35 -0600

    corrections

--- src/main/Arrays.java
+++ src/main/Arrays.java
@@ -9,20 +9,10 @@ public class Arrays {
     public static void main(String[] args) {
         //Initiate an Arraylist. Using For loop add integers from 1 to 10
         ArrayList<Integer> num = new ArrayList<Integer>();
-        num.add(1);
-        num.add(2);
-        num.add(3);
-        num.add(4);
-        num.add(5);
-        num.add(6);
-        num.add(7);
-        num.add(8);
-        num.add(9);
-       num.add(10);
-        for (int i = 0; i < 10; i++) {
-           System.out.println(num.get(i));
+        for (int i = 0; i <= 10; i++) {
+          System.out.println(i);
         }
-      //Create an ArrayList [1,2,3,6,7,3,2,1]. Remove all 2 from Arraylist
+        //Create an ArrayList [1,2,3,6,7,3,2,1]. Remove all 2 from Arraylist
         ArrayList<Integer> two = new ArrayList<>();
         two.add(1);
         two.add(2);
@@ -32,31 +22,32 @@ public class Arrays {
         two.add(3);
         two.add(2);
         two.add(1);
-        for (int i = 0; i<two.size(); i++){
+        for (int i =0;i<two.size();i++){
             if (two.get(i)==2){
-                System.out.println("");
+                two.remove(i);
             }
-            else{
-               System.out.println(two.get(i));
-            }
+            System.out.println(two);
         }
-        //Remove all odd numbers;
+
+
+
+        //Remove all odd numbers; With remove method it didnt work out(
         ArrayList<Integer> list = new ArrayList<>();
         list.add(2);
         list.add(3);
         list.add(4);
         list.add(7);
-        for (int i = 0; i<list.size(); i++){
-            if (i % 2 != 0){
-                System.out.println(list.get(i));
+        ArrayList<Integer> list2 = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) % 2 != 0) {
+                list2.add(list.get(i));
             }
-            else{
-                System.out.println("");
-            }
+            System.out.println(list2);
         }
 
-
+        }
     }
-}
+
+
 
 
